19e7dd142255ab443eb9d9db5082c49160500f1e
swastik1992 <swastikswarnkar@gmail.com>
2019-10-14 23:42:12 +0800

    updated opengl setup.

--- BreakoutGame/BreakoutGame/Source.cpp
+++ BreakoutGame/BreakoutGame/Source.cpp
@@ -2,104 +2,75 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <GL\glew.h>
+//#include <GL\glew.h>
 #include <GL\freeglut.h>
+#include <GLFW\glfw3.h>
 
-#define WINDOW_TITLE_PREFIX "Something"
+const GLuint SCREEN_WIDTH = 800;
+const GLuint SCREEN_HEIGHT = 600;
 
-int
-CurrentWidth = 800,
-CurrentHeight = 600,
-WindowHandle = 0;
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
-void Initialize(int, char*[]);
-void InitWindow(int, char*[]);
-void ResizeFunction(int, int);
-void RenderFunction(void);
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	Initialize(argc, argv);
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	glutMainLoop();
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Breakout", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
 
-	//exit(EXIT_SUCCESS);
+	glfwSetKeyCallback(window, key_callback);
+
+	// OpenGL configuration
+	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	glEnable(GL_CULL_FACE);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	// Initialize game
+	
+
+	// DeltaTime variables
+	GLfloat deltaTime = 0.0f;
+	GLfloat lastFrame = 0.0f;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// Calculate delta time
+		GLfloat currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		glfwPollEvents();
+
+		//deltaTime = 0.001f;
+		// Manage user input
+		
+
+		// Update Game state
+		
+
+		// Render
+		glClearColor(0.0f, 0.50f, 0.20f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+
+
+		glfwSwapBuffers(window);
+	}
+
+	// Delete all resources as loaded using the resource manager
+	//ResourceManager::Clear();
+
+	glfwTerminate();
 	return 0;
 }
 
-void Initialize(int argc, char* argv[])
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
-	InitWindow(argc, argv);
 
-	fprintf(
-		stdout,
-		"INFO: OpenGL Version: %s\n",
-		glGetString(GL_VERSION)
-	);
-
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 }
-
-void InitWindow(int argc, char* argv[])
-{
-	glutInit(&argc, argv);
-
-	glutInitContextVersion(4, 0);
-	glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
-	glutInitContextProfile(GLUT_CORE_PROFILE);
-
-	glutSetOption(
-		GLUT_ACTION_ON_WINDOW_CLOSE,
-		GLUT_ACTION_GLUTMAINLOOP_RETURNS
-	);
-
-	glutInitWindowSize(CurrentWidth, CurrentHeight);
-
-	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-
-	WindowHandle = glutCreateWindow(WINDOW_TITLE_PREFIX);
-
-	if (WindowHandle < 1) {
-		fprintf(
-			stderr,
-			"ERROR: Could not create a new rendering window.\n"
-		);
-		exit(EXIT_FAILURE);
-	}
-
-	glutReshapeFunc(ResizeFunction);
-	glutDisplayFunc(RenderFunction);
-}
-
-void ResizeFunction(int Width, int Height)
-{
-	CurrentWidth = Width;
-	CurrentHeight = Height;
-	glViewport(0, 0, CurrentWidth, CurrentHeight);
-}
-
-void RenderFunction(void)
-{
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glutSwapBuffers();
-	glutPostRedisplay();
-}
-//#inclide "GL\"
-//
-//
-//int main(int argc, char* argv[])
-//{
-//	SDL_Init(SDL_INIT_VIDEO);
-//
-//	SDL_Window* window = SDL_CreateWindow
-//	("An SDL2 window", // window's title
-//		10, 25, // coordinates on the screen, in pixels, of the window's upper left corner
-//		640, 480, // window's length and height in pixels  
-//		SDL_WINDOW_OPENGL);
-//
-//	SDL_Delay(3000); // window lasts 3 seconds
-//	SDL_DestroyWindow(window);
-//	SDL_Quit();
-//	return 0;
-//}
